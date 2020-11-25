26ab43d297a3f1cbbf8f03245e8a1c897f8a65b6
AaronHuerta25 <anewstar255@gmail.com>
2020-11-25 08:44:33 -0600

    main.bat created -- first ver

---
+++ main.bat
@@ -0,0 +1,12 @@
+@echo off
+echo ---------------------------------
+
+echo Hello I will try to list your files
+
+echo ---------------------------------
+dir 
+echo ---------------------------------
+
+echo Done! have a nice day
+
+echo ---------------------------------
